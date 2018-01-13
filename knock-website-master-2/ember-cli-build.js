4bb9917182c2484acb9314c15565ebd10700c088
sdmitrash <ronald1nh0@bk.ru>
2018-01-13 14:17:01 +0200

    x

--- knock-website-master-2/ember-cli-build.js
+++ knock-website-master-2/ember-cli-build.js
@@ -8,21 +8,21 @@ module.exports = function(defaults) {
     // Add options here
   });
 
-  app.import('vendor/bootstrap.min.css');
-  app.import('vendor/style.css');
-  app.import('vendor/responsive.css');
-  app.import('vendor/fonts/Roboto-Black_0.ttf');
-  app.import('vendor/fonts/Roboto-BlackItalic_0.ttf');
-  app.import('vendor/fonts/Roboto-Bold_0.ttf');
-  app.import('vendor/fonts/Roboto-BoldItalic_0.ttf');
-  app.import('vendor/fonts/Roboto-Medium_0.ttf');
-  app.import('vendor/fonts/Roboto-MediumItalic_0.ttf');
-  app.import('vendor/fonts/Roboto-Regular_0.ttf');
-  app.import('vendor/fonts/Roboto-Italic_0.ttf');
-  app.import('vendor/fonts/Roboto-Light_0.ttf');
-  app.import('vendor/fonts/Roboto-LightItalic_0.ttf');
-  app.import('vendor/fonts/Roboto-Thin_0.ttf');
-  app.import('vendor/fonts/Roboto-ThinItalic_0.ttf');
+  // app.import('vendor/bootstrap.min.css');
+  // app.import('vendor/style.css');
+  // app.import('vendor/responsive.css');
+  // app.import('vendor/fonts/Roboto-Black_0.ttf');
+  // app.import('vendor/fonts/Roboto-BlackItalic_0.ttf');
+  // app.import('vendor/fonts/Roboto-Bold_0.ttf');
+  // app.import('vendor/fonts/Roboto-BoldItalic_0.ttf');
+  // app.import('vendor/fonts/Roboto-Medium_0.ttf');
+  // app.import('vendor/fonts/Roboto-MediumItalic_0.ttf');
+  // app.import('vendor/fonts/Roboto-Regular_0.ttf');
+  // app.import('vendor/fonts/Roboto-Italic_0.ttf');
+  // app.import('vendor/fonts/Roboto-Light_0.ttf');
+  // app.import('vendor/fonts/Roboto-LightItalic_0.ttf');
+  // app.import('vendor/fonts/Roboto-Thin_0.ttf');
+  // app.import('vendor/fonts/Roboto-ThinItalic_0.ttf');
 
   return app.toTree();
 };
